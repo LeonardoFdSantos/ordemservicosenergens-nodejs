fe9b4bab2a079d1f1322efe3a359fe2b358cd1ae
Leonardo Felipe - Samsung <leosan95@live.com>
2021-09-19 15:16:22 -0300

    Atualização criação da tabela cliente, modelo, controllers e rotas.cls

--- app/models/clientes.model.js
+++ app/models/clientes.model.js
@@ -4,39 +4,51 @@ module.exports = (sequelize, Sequelize) => {
             type: Sequelize.INTEGER,
             autoIncrement: true,
             primaryKey: true,
+            allowNull: false
         },
         nome: {
             type: Sequelize.STRING,
+            allowNull: false
         },
         codigoUC: {
             type: Sequelize.STRING,
+            allowNull: false
         },
         UCsCompensacao: {
             type: Sequelize.JSONB,
+            allowNull: true
         },
         localizacao: {
             type: Sequelize.STRING,
+            allowNull: false
         },
         CEP: {
             type: Sequelize.STRING,
+            allowNull: false
         },
         Bairro: {
             type: Sequelize.STRING,
+            allowNull: false
         },
         Email: {
             type: Sequelize.STRING,
+            allowNull: false
         },
         Telefone: {
             type: Sequelize.STRING,
+            allowNull: false
         },
         CPF_CNPJ: {
             type: Sequelize.STRING,
+            allowNull: false
         },
         ValorFormaPagamento: {
             type: Sequelize.TEXT,
+            allowNull: true
         },
         Notas:{
             type: Sequelize.TEXT,
+            allowNull: true
         }
     }
     ,{
